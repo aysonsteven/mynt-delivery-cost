4b49fea3af1f5e48127f753e5f70645377e7df0f
aysonsteven <aysonsteven@gmail.com>
2024-05-07 08:51:07 +0800

    add unit tests

--- src/test/java/com/mynt/delivarycostcalculator/testconfig/TestConfig.java
+++ src/test/java/com/mynt/delivarycostcalculator/testconfig/TestConfig.java
@@ -2,9 +2,12 @@ package com.mynt.delivarycostcalculator.testconfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 import com.mynt.delivarycostcalculator.service.ParcelService;
+import com.mynt.delivarycostcalculator.service.VoucherService;
 import com.mynt.delivarycostcalculator.service.impl.ParcelServiceImpl;
+import com.mynt.delivarycostcalculator.service.impl.VoucherServiceImpl;
 
 @Configuration
 public class TestConfig {
@@ -12,6 +15,16 @@ public class TestConfig {
 	@Bean
 	public ParcelService parcelService(){
 		return new ParcelServiceImpl();
+	}
+	
+	@Bean
+	public VoucherService voucherService() {
+		return new VoucherServiceImpl();
+	}
+	
+	@Bean
+	public RestTemplate restTemplate() {
+	    return new RestTemplate();
 	}
 
 }
--- src/test/java/com/mynt/delivarycostcalculator/vouchertest/VoucherControllerTest.java
+++ src/test/java/com/mynt/delivarycostcalculator/vouchertest/VoucherControllerTest.java
@@ -1,69 +1,74 @@
-//package com.mynt.delivarycostcalculator.vouchertest;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.mockito.Mockito.mock;
-//import static org.mockito.Mockito.when;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.InjectMocks;
-//import org.mockito.MockitoAnnotations;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.MediaType;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-//import org.springframework.http.ResponseEntity;
-//
-//import com.mynt.delivarycostcalculator.config.DeliveryConfig;
-//import com.mynt.delivarycostcalculator.controller.VoucherController;
-//import com.mynt.delivarycostcalculator.dto.Voucher;
-//import com.mynt.delivarycostcalculator.dto.VoucherItem;
-//import com.mynt.delivarycostcalculator.service.VoucherService;
-//
-//
-//
-//public class VoucherControllerTest {
-//
-//
-//	private VoucherService voucherService = mock(VoucherService.class);
-//	
-//    @Autowired
-//    private MockMvc mockMvc;
-//    
-//    @MockBean
-//    private DeliveryConfig deliveryConfig;
-//    
-//    @InjectMocks
-//    private VoucherController voucherController;
-//    
-//    @BeforeEach
-//    public void setup() {
-//        MockitoAnnotations.openMocks(this);
-//    }
-//    
-//    @Test
-//    public void testVerifyVoucherDetails() throws Exception {
-//        // Mock voucher details
-//        String voucherCode = "1";
-//        VoucherItem item = new VoucherItem("1", 20.0, "2024-05-03T23:38:37.113Z");
-//        Voucher voucher = new Voucher(item, null, true);
-//        // Stubbing the behavior of VoucherService
-//        when(voucherService.getVoucherDetails(voucherCode)).thenReturn(voucher);
-//
-//        // Call controller method
-//        mockMvc.perform(MockMvcRequestBuilders.post("/cost")
-//                .content(voucherCode)
-//                .contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.status").value("Success"))
-//                .andExpect(jsonPath("$.voucherItem").value(item))
-//                .andExpect(jsonPath("$.isExpired").value(true)); // Adjust expected result based on your test case
-//  
-//    }
-//
-//}
+package com.mynt.delivarycostcalculator.vouchertest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.http.ResponseEntity;
+
+import com.mynt.delivarycostcalculator.config.DeliveryConfig;
+import com.mynt.delivarycostcalculator.controller.ParcelController;
+import com.mynt.delivarycostcalculator.controller.VoucherController;
+import com.mynt.delivarycostcalculator.dto.Voucher;
+import com.mynt.delivarycostcalculator.dto.VoucherItem;
+import com.mynt.delivarycostcalculator.service.VoucherService;
+import com.mynt.delivarycostcalculator.testconfig.TestConfig;
+
+
+@WebMvcTest(controllers = VoucherController.class)
+@ContextConfiguration(classes = {TestConfig.class})
+public class VoucherControllerTest {
+
+
+	private VoucherService voucherService = mock(VoucherService.class);
+	
+    @Autowired
+    private MockMvc mockMvc;
+    
+    @MockBean
+    private DeliveryConfig deliveryConfig;
+    
+    @InjectMocks
+    private VoucherController voucherController;
+    
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+    
+    @Test
+    public void testVerifyVoucherDetails() throws Exception {
+        // Mock voucher details
+        String voucherCode = "1";
+        VoucherItem item = new VoucherItem("1", 20.0, "2024-05-03T23:38:37.113Z");
+        Voucher voucher = new Voucher(item, null, true);
+        // Stubbing the behavior of VoucherService
+        when(voucherService.getVoucherDetails(voucherCode)).thenReturn(voucher);
+
+        // Call controller method
+        mockMvc.perform(MockMvcRequestBuilders.post("/cost")
+                .content(voucherCode)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("Success"))
+                .andExpect(jsonPath("$.voucherItem").value(item))
+                .andExpect(jsonPath("$.isExpired").value(true)); // Adjust expected result based on your test case
+  
+    }
+
+}
